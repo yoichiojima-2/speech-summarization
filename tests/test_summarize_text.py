42dc657e6e18ffbb00836de5459fef7b6975a902
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-09 15:09:53 +0900

    remove unused variables and packages

--- tests/test_summarize_text.py
+++ tests/test_summarize_text.py
@@ -1,9 +1,11 @@
-import sys
 from pathlib import Path
 from audio_summarization import summarize_text
 
 
 def test_summarize_text():
     output = Path("./data/summarized_text.txt")
+    if output.exists():
+        output.unlink()
+    summarize_text.main("./tests/data/test.txt")
     text = output.read_text()
     assert text
