57d22249ac1662c44d9c13e4d58dde3defacb96a
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-09 15:03:52 +0900

    organize code / simplify test

--- audio_summarization/summarize_text.py
+++ audio_summarization/summarize_text.py
@@ -1,7 +1,6 @@
 import os
 import argparse
 from pathlib import Path
-import openai
 from openai import OpenAI
 
 
@@ -14,15 +13,15 @@ def main(input_path: str):
     input_text = input_path.read_text()
 
     response = client.chat.completions.create(
-        messages = [
+        messages=[
             {"role": "user", "content": "summarize this text"},
             {"role": "user", "content": input_text},
         ],
-        model = "gpt-4"
+        model="gpt-4",
     )
 
     target_dir = Path("./data")
-    target_dir.mkdir(parents = True, exist_ok = True)
+    target_dir.mkdir(parents=True, exist_ok=True)
     (target_dir / "summarized_text.txt").write_text(response.choices[0].message.content)
 
 
@@ -31,7 +30,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--input-path")
     return parser.parse_args()
 
+
 if __name__ == "__main__":
     args = parse_args()
-    main(input_path = args.input_path)
-    
+    main(input_path=args.input_path)
--- tests/test_summarize_text.py
+++ tests/test_summarize_text.py
@@ -1,13 +1,9 @@
 import sys
 from pathlib import Path
+from audio_summarization import summarize_text
 
-app_root = Path("./audio_summarization")
-sys.path.append(str(app_root))
-
-import summarize_text
 
 def test_summarize_text():
-    res = summarize_text.main(input_path = "./tests/data/test.txt")
     output = Path("./data/summarized_text.txt")
     text = output.read_text()
     assert text
