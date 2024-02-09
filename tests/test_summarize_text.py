3c27c43e0dc1617eb60281ca748acee5d8ef32e1
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-09 15:44:35 +0900

    update reading path

--- tests/test_summarize_text.py
+++ tests/test_summarize_text.py
@@ -3,9 +3,9 @@ from audio_summarization import summarize_text
 
 
 def test_summarize_text():
-    output = Path("./data/summarized_text.txt")
+    output = Path("./data/summarize_text/test.txt")
     if output.exists():
         output.unlink()
-    summarize_text.main("./tests/data/test.txt")
+    summarize_text.main("./tests/data/input/test.txt")
     text = output.read_text()
     assert text
