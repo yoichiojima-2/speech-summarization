a1183e5fa0e189cf7dba378a3e28cc4d559fa7cd
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-11 10:16:03 +0900

    add main.py

---
+++ audio_summarization/main.py
@@ -0,0 +1,11 @@
+import audio2text
+import summarize_text
+
+
+def main():
+    # uncomment
+    # audio2text.main(input_path = "./data/input/schoo.wav")
+    summarize_text.main(input_path  = "./data/audio2text/test.txt")
+
+if __name__== "__main__":
+    main()
