75a8e3597f3758f96f338693cd19a14348a2275d
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-13 11:59:43 +0900

    simplify main.py

--- audio_summarization/main.py
+++ audio_summarization/main.py
@@ -1,11 +1,5 @@
-import audio2text
-import summarize_text
-
-
 def main():
-    # uncomment
-    # audio2text.main(input_path = "./data/input/schoo.wav")
-    summarize_text.main(input_path  = "./data/audio2text/test.txt")
+    pass
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
