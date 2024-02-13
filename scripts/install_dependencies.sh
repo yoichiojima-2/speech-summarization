2442593834742c436eb09a13b7d56004cab0cf20
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-13 19:17:49 +0900

    upgrade python version

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+poetry add openai
+poetry add --group dev pytest
+poetry add --group dev black
+poetry add --group dev isort
+poetry add --group dev flake8
+poetry add --group dev ipython
