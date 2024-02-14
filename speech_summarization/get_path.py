e3eb0ded90452145b1cb69659de321a0895d6228
yo <145314115+yoichiojima-2@users.noreply.github.com>
2024-02-14 21:31:30 +0900

    Remove unnecessary parentheses

--- speech_summarization/get_path.py
+++ speech_summarization/get_path.py
@@ -4,7 +4,7 @@ def app_root() -> Path:
     return Path().cwd()
 
 def speech2text(target: Path) -> Path:
-    return (app_root() / f"data/speech2text/{Path(target).stem}.txt")
+    return app_root() / f"data/speech2text/{Path(target).stem}.txt"
 
 def split_text(target: Path) -> Path:
-    return (app_root() / f"data/split_text/{Path(target).stem}")
+    return app_root() / f"data/split_text/{Path(target).stem}"
