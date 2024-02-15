517ba4f1d035f3d9c50af94e9222cccb961ef469
yo <145314115+yoichiojima-2@users.noreply.github.com>
2024-02-15 16:23:16 +0900

    Fix wrong type hints

--- speech_summarization/get_path.py
+++ speech_summarization/get_path.py
@@ -5,13 +5,13 @@ def app_root() -> Path:
     return Path().cwd()
 
 
-def speech2text(target: Path) -> Path:
+def speech2text(target: str) -> Path:
     return app_root() / f"data/speech2text/{Path(target).stem}.txt"
 
 
-def split_text(target: Path) -> Path:
+def split_text(target: str) -> Path:
     return app_root() / f"data/split_text/{Path(target).stem}"
 
 
-def summarize_text(target: Path) -> Path:
+def summarize_text(target: str) -> Path:
     return app_root() / f"data/summarize_text/{Path(target).stem}"
