bd16c61d1f816ad861a7a643b0438f5c9b6b8010
Yoichi Ojima <yoichiojima@gmail.com>
2024-02-15 14:38:14 +0900

    update invoke.sh

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -2,14 +2,14 @@
 
 mkdir -p ./logs
 
-input_directory="./tests/data/input"
+input_directory="./data/input"
 
 for file in $input_directory/*; do
-  echo "Processing $file"
   filename=$(basename $file)
   filename_wo_ext="${filename%.*}"
+  echo "Processing $file"
   poetry run python \
-    ./speech_summarization/pipeline.py SplitText \
-    --target $file \
+    ./speech_summarization/pipeline.py SummarizeText \
+    --target "${file}" \
     > "./logs/${filename_wo_ext}.log" 2>&1
 done
